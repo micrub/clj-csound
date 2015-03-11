e69689c969a70fcfd6b96242fca50de062f2a165
Michael Rubanov <mrubanov@gmail.com>
2015-03-11 22:57:39 +0200

    @init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-csound
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_csound/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-csound.core-test
+  (:require [clojure.test :refer :all]
+            [clj-csound.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
